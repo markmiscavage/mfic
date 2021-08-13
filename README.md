08a6b12f0091f0097bacdeeac9a73dff869fa39d
markmiscavage <mark.miscavage@wonderful.com>
2021-08-13 09:01:10 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Build CSS
+`npx tailwindcss -o tailwind.css`
+
+Build Minified CSS
+`NODE_ENV=production npx tailwindcss -i ./tailwind.css -o ./tailwind.min.css --minify`
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#3583a6',
+        'primary-80': '#255c74',
+        'primary-70': '#2a6985',
+        'primary-60': '#307695',
+        'primary-50': '#498faf',
+        'primary-40': '#5d9cb8',
+        'primary-30': '#72a8c1',
+
+      },
+      boxShadow: {
+        linebreak: '0 10px 10px -10px rgba(96, 90, 80, 1)',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
